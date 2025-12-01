76c60308af308d2ca74c66b340b1a2439fcd23ff
RafaelSilvaScrippter <nativaeleven@gmail.com>
2025-12-01 17:08:06 -0300

    Tabela user criada

--- api/auth.mjs
+++ api/auth.mjs
@@ -1,5 +1,21 @@
+import { DbConnect } from "../cors/connectDatabase.mjs";
+import { tableUsers } from "./tables.mjs";
+
 export class Auth {
+  constructor() {
+    this.dataBase = new DbConnect().dbInit();
+    this.init();
+  }
   postUser(req, res) {
     res.end("usuarios");
+  }
+
+  db() {
+    console.log("oi");
+    this.dataBase.exec(tableUsers);
+  }
+
+  init() {
+    this.db();
   }
 }
