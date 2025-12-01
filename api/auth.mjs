65cb6c90834c137b0596ec9040d18b0b081b1641
RafaelSilvaScrippter <nativaeleven@gmail.com>
2025-12-01 20:05:31 -0300

    Rotas refatorada

--- api/auth.mjs
+++ api/auth.mjs
@@ -1,21 +1,29 @@
 import { DbConnect } from "../cors/connectDatabase.mjs";
+import { Routes } from "../cors/routes.mjs";
 import { tableUsers } from "./tables.mjs";
 
 export class Auth {
   constructor() {
     this.dataBase = new DbConnect().dbInit();
     this.init();
+    this.rotacionar = new Routes();
   }
   postUser(req, res) {
     res.end("usuarios");
   }
 
   db() {
-    console.log("oi");
     this.dataBase.exec(tableUsers);
   }
 
-  init() {
+  routes(req, res) {
+    if (this.rotacionar) {
+      this.rotacionar.get("/user", this.postUser(req, res));
+    }
+  }
+
+  init(req, res) {
     this.db();
+    this.routes(req, res);
   }
 }
