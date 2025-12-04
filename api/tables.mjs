3e226207f4b9688dbaba1e685b24c61714539a6f
RafaelSilvaScrippter <nativaeleven@gmail.com>
2025-12-04 17:14:56 -0300

    Criada a regra do usuário

--- api/tables.mjs
+++ api/tables.mjs
@@ -6,21 +6,23 @@ export const tableUsers =
     "user_id" INTEGER,
     "session_hash" TEXT NOT NULL,
     "revoked" TEXT NOT NULL DEFAULT 0 CHECK ("revoked" IN (0,1)),
-    FOREIGN KEY ("user_id") REFERENCES "users" ("user_id")
+    FOREIGN KEY ("user_id") REFERENCES "users" ("user_id") ON DELETE CASCADE
   );
   CREATE TABLE IF NOT EXISTS "users"
   ("user_id" INTEGER PRIMARY KEY,
     "name" TEXT NOT NULL,
     "second_name" TEXT NOT NULL,
     "email" TEXT UNIQUE NOT NULL,
-    "senha" TEXT NOT NULL,
+    "role" TEXT DEFAULT "user" CHECK ("role" = 'admin' or "role" = 'user'),
+    "password" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
     "CPF" INTEGER UNIQUE NOT NULL
   );
   CREATE TABLE IF NOT EXISTS "reset_password"
   ("id" INTEGER PRIMARY KEY,
   "user_id" INTEGER NOT NULL,
   "token" TEXT NOT NULL,
-  FOREIGN KEY ("user_id") REFERENCES "users" ("user_id")
+  FOREIGN KEY ("user_id") REFERENCES "users" ("user_id") ON DELETE CASCADE
   )
 
 `;
