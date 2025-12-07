be29289f4361a36224d16fa8f4c41684de8c5e2d
RafaelSilvaScrippter <nativaeleven@gmail.com>
2025-12-07 14:54:45 -0300

    Cancelando compra de produto

--- api/admin/query.mjs
+++ api/admin/query.mjs
@@ -3,7 +3,7 @@ export class Query {
     this.db = db;
   }
 
-  selectProducReversal() {
+  selectProductsReversal() {
     return this.db
       .prepare(
         /*sql */ `
@@ -13,5 +13,42 @@ export class Query {
     `
       )
       .all();
+  }
+
+  selectProductReversal({ id }) {
+    return this.db
+      .prepare(
+        /*sql */ `
+    
+      SELECT "rv".*,"u"."name","p"."price","p"."name","p"."photo" FROM "reversal" AS "rv"
+      INNER JOIN "users" AS "u" ON "rv"."user_id" = "u"."user_id" 
+      INNER JOIN "products" AS "p" ON "rv"."product_id" = "p"."id"
+      WHERE "p"."id" = ?
+    `
+      )
+      .get(id);
+  }
+  selectBuyProducts({ id }) {
+    return this.db
+      .prepare(
+        /*sql */ `
+    
+      SELECT * FROM "buyProducts"
+      WHERE "product_buy" = ?
+      
+    `
+      )
+      .get(id);
+  }
+  reversalUpdateBuyProduct({ id }) {
+    return this.db
+      .prepare(
+        /*sql */ `
+      UPDATE "buyProducts" 
+      SET "status" = 'cancelado'
+      WHERE "product_buy" = ?
+    `
+      )
+      .run(id);
   }
 }
